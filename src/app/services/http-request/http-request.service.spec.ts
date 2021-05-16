c7713f979863d2cf9e5d1c1c91a9fb8567f1e393
Neo Tshoma <neosellotshoma@gmail.com>
2021-05-16 20:19:50 +0200

    Updated all unit tests

--- src/app/services/http-request/http-request.service.spec.ts
+++ src/app/services/http-request/http-request.service.spec.ts
@@ -1,12 +1,43 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { HttpRequestService } from './http-request.service';
 
 describe('HttpRequestService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  const dataServiceMock = jasmine.createSpyObj('HttpRequestService', [
+    'getAllRequest',
+    'getByIdRequest'
+  ]);
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule],
+    providers: [HttpClient]
+  }));
 
   it('should be created', () => {
     const service: HttpRequestService = TestBed.get(HttpRequestService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get data using the provided url', () => {
+    const service: HttpRequestService = TestBed.get(HttpRequestService);
+    const httpRequest: HttpClient = TestBed.get(HttpClient);
+
+    spyOn(httpRequest, 'get');
+
+    service.getAllRequest('testUrl');
+
+    expect(httpRequest.get).toHaveBeenCalled();
+  });
+
+  it('should get data using the provided uri data', () => {
+    const service: HttpRequestService = TestBed.get(HttpRequestService);
+    const httpRequest: HttpClient = TestBed.get(HttpClient);
+
+    spyOn(httpRequest, 'get');
+
+    service.getByIdRequest('uri1', 'uri2');
+
+    expect(httpRequest.get).toHaveBeenCalled();
   });
 });
